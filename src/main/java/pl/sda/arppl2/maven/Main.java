399a17b8e27e520823e2e19b718a1d5cc1b8c326
Alicja Mojska <Alicja Mojska>
2022-04-03 13:49:08 +0200

    first commit

---
+++ src/main/java/pl/sda/arppl2/maven/Main.java
@@ -0,0 +1,12 @@
+package pl.sda.arppl2.maven;
+
+public class Main {
+    //psvm - public static void main
+    //generuje metode startowa. jest to metoda niezbedna do rozpoczecia obslugi programu
+    public static void main(String[] args) {
+//generuje komende wypisania do konsoli
+        //sout -> system.out.println();
+        //uzyj systemowego wyjscia, wypisz linie (to co w nawiasie)
+        System.out.println("Hello World!");
+    }
+}
